20a9b32b4ce429690d6a1535641a92487c909bed
jilian <jeustage@gmail.com>
2025-12-06 13:09:26 +0100

    feature : Modification du cameraReader pour utiliser le standard BGR employé par OpenCV

--- src/camera_reader/camera_reader/reader.py
+++ src/camera_reader/camera_reader/reader.py
@@ -1,12 +1,13 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image 
+from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge 
-import math
 import json
 import depthai as dai
 import numpy as np
 import os
+import cv2
+import threading 
 from ament_index_python.packages import get_package_share_directory
 from .yolo_api import Segment 
 
@@ -14,38 +15,34 @@ class CameraReader(Node):
     def __init__(self):
         super().__init__('camera_reader_node')
 
-        self.get_logger().info('CameraReader node started and initializing DepthAI...')
-
+        self.get_logger().info('CameraReader node started...')
         self.package_share_directory = get_package_share_directory('camera_reader')
 
-
+        # --- Load Config ---
         try:
             config_path = os.path.join(self.package_share_directory, 'data', 'config.json')
             with open(config_path, "r") as config:
                 self.model_data = json.load(config)
-                pass
-                
         except Exception as e:
-            self.get_logger().error(f"Erreur lors du chargement de la configuration: {e}")
+            self.get_logger().error(f"Erreur config: {e}")
+            return
 
         self.preview_img_width = self.model_data["input_width"]
         self.preview_img_height = self.model_data["input_height"]
         self.input_shape = [1, 3, self.preview_img_height, self.preview_img_width]
         
+        # --- Load Blob ---
         try:
             blob_filename = "yolo11n-seg640x640.blob"
             self.path_to_yolo_blob = os.path.join(self.package_share_directory, 'models', blob_filename)
-            
-            self.get_logger().info(f"Chemin du Blob défini à: {self.path_to_yolo_blob}")
-            
         except Exception as e:
-            self.get_logger().error(f"Erreur lors de la détermination du chemin du Blob : {e}")
+            self.get_logger().error(f"Erreur Blob path: {e}")
             raise
 
-        # --- 2. INIT DEPTHAI PIPELINE AND CLASSES ---
+        # --- Init Pipeline ---
         self._init_depthai_pipeline()
         
-        # 3. INIT DE Segment CLASSE
+        # --- Init Segment ---
         self.yoloseg = Segment(
             input_shape=self.input_shape,
             input_height=self.preview_img_height,
@@ -56,16 +53,20 @@ class CameraReader(Node):
         )
         self.yoloseg.prepare_input_for_oakd((self.preview_img_height, self.preview_img_width))
 
-        # --- 4. INIT ROS 2 PUBLISHER ---
-        self.seg_publisher_ = self.create_publisher(Image, 'segmentation/image_raw', 10)
-        self.image_publisher_ = self.create_publisher(Image, 'segmentation/through/image_raw', 10)
+        # --- Init ROS ---
+        self.seg_publisher_ = self.create_publisher(Image, 'segmentation/image_raw', 2)
+        self.image_publisher_ = self.create_publisher(Image, 'segmentation/through/image_raw', 2)
+        self.seg_compressed_publisher_ = self.create_publisher(CompressedImage, 'segmentation/image_raw/compressed', 2)
+        self.image_compressed_publisher_ = self.create_publisher(CompressedImage, 'segmentation/through/image_raw/compressed', 2)
         self.bridge = CvBridge()
-        self.timer_period = 0.033 
-        self.timer = self.create_timer(self.timer_period, self._timer_callback)
-        self.get_logger().info('DepthAI pipeline launched. Ready to publish segmentation results.')
+        
+        self.running = True
+        self.thread = threading.Thread(target=self._run_camera_loop)
+        self.thread.start()
+        
+        self.get_logger().info('DepthAI thread started. Ready.')
 
     def _init_depthai_pipeline(self):
-
         self.device = dai.Device()
         self.pipeline = dai.Pipeline()
 
@@ -74,56 +75,69 @@ class CameraReader(Node):
         cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_800_P)
         cam_rgb.setPreviewSize(self.preview_img_width, self.preview_img_height)
         cam_rgb.setInterleaved(False)
+        cam_rgb.setFps(30)
         cam_rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
 
         nn = self.pipeline.create(dai.node.NeuralNetwork)
         nn.setBlobPath(self.path_to_yolo_blob)
+        nn.input.setBlocking(False) 
+        
         cam_rgb.preview.link(nn.input)
 
         xout_rgb = self.pipeline.create(dai.node.XLinkOut)
         xout_rgb.setStreamName("rgb_stream")
-        cam_rgb.preview.link(xout_rgb.input) 
+        nn.passthrough.link(xout_rgb.input) 
 
         xout_nn = self.pipeline.create(dai.node.XLinkOut)
         xout_nn.setStreamName("nn_results")
-        
         nn.out.link(xout_nn.input)
 
         self.device.startPipeline(self.pipeline)
 
-        self.q_rgb = self.device.getOutputQueue(name="rgb_stream", maxSize=4, blocking=False)
-        self.q_yolo = self.device.getOutputQueue(name="nn_results", maxSize=4, blocking=False)
-        
-        self.get_logger().info('DepthAI device and pipeline configured.')
+        self.q_rgb = self.device.getOutputQueue(name="rgb_stream", maxSize=2, blocking=False)
+        self.q_yolo = self.device.getOutputQueue(name="nn_results", maxSize=2, blocking=False)
 
+    def _run_camera_loop(self):
+        """Ce code tourne en parallèle dans un thread séparé"""
+        while self.running and rclpy.ok():
+            try:
+                in_nn = self.q_yolo.get() 
+                in_rgb = self.q_rgb.get() 
 
-    def _timer_callback(self):
-        try:
-            in_rgb = self.q_rgb.tryGet()
-            in_nn = self.q_yolo.tryGet()
+                if in_rgb is not None and in_nn is not None:
+                    frame = in_rgb.getCvFrame()
+                    now = self.get_clock().now().to_msg()
 
-            if in_rgb is not None and in_nn is not None:
-                frame = in_rgb.getCvFrame()
+                    ros_image_msg = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+                    ros_image_msg.header.stamp = now
+                    self.image_publisher_.publish(ros_image_msg)
 
-                ros_image_msg = self.bridge.cv2_to_imgmsg(frame, encoding="rgb8")
-                ros_image_msg.header.stamp = self.get_clock().now().to_msg()
-                self.image_publisher_.publish(ros_image_msg)
-                
-                layer0 = np.array(in_nn.getLayerFp16("output0")).reshape(self.model_data["shapes"]["output0"])
-                layer1 =np.array( in_nn.getLayerFp16("output1")).reshape(self.model_data["shapes"]["output1"])
+                    ros_image_msg_compressed = self.bridge.cv2_to_compressed_imgmsg(frame, dst_format='jpg')
+                    ros_image_msg_compressed.header.stamp = now
+                    self.image_compressed_publisher_.publish(ros_image_msg_compressed)
 
-                self.yoloseg.segment_objects_from_oakd(layer0, layer1)
+                    layer0 = np.array(in_nn.getLayerFp16("output0")).reshape(self.model_data["shapes"]["output0"])
+                    layer1 = np.array(in_nn.getLayerFp16("output1")).reshape(self.model_data["shapes"]["output1"])
 
-                display_frame = self.yoloseg.draw_masks(frame, draw_scores=True, mask_alpha=0.5)
+                    self.yoloseg.segment_objects_from_oakd(layer0, layer1)
 
-                ros_image_msg = self.bridge.cv2_to_imgmsg(display_frame, encoding="rgb8")
-                ros_image_msg.header.stamp = self.get_clock().now().to_msg()
-                self.seg_publisher_.publish(ros_image_msg)
+                    display_frame = self.yoloseg.draw_masks(frame, draw_scores=True, mask_alpha=0.5)
 
-        except Exception as e:
-            self.get_logger().error(f"Erreur traitement dans le callback: {e}")
+                    seg_msg = self.bridge.cv2_to_imgmsg(display_frame, encoding="bgr8")
+                    seg_msg.header.stamp = now
+                    self.seg_publisher_.publish(seg_msg)
+
+                    seg_msg_compressed = self.bridge.cv2_to_compressed_imgmsg(display_frame, dst_format='jpg')
+                    seg_msg_compressed.header.stamp = now
+                    self.seg_compressed_publisher_.publish(seg_msg_compressed)
+
+            except Exception as e:
+                self.get_logger().error(f"Erreur thread caméra: {e}")
 
     def destroy_node(self):
+        self.running = False
+        if hasattr(self, 'thread'):
+            self.thread.join()
         super().destroy_node()
 
 def main(args=None):
@@ -131,6 +145,8 @@ def main(args=None):
     node = CameraReader()
     try:
         rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
     finally:    
         node.destroy_node()
         rclpy.shutdown()
