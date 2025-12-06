47626b1390bfb60b130d66a618d1c613c27f50ae
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2025-12-06 17:13:44 +0100

    feat: Adding the goal point topic

--- src/camera_reader/launch/camera.launch.py
+++ src/camera_reader/launch/camera.launch.py
@@ -1,20 +1,12 @@
-import os
 from launch import LaunchDescription
-# L'import de Node est essentiel pour lancer un exécutable
 from launch_ros.actions import Node 
 
-
 def generate_launch_description():
-    
     return LaunchDescription([
-        
-        # 1. LANCER LE SCRIPT READER.PY EN TANT QUE NŒUD ROS 2
         Node(
-            package="camera_reader",  # Votre package ROS 2
-            executable="reader_node", # L'exécutable déclaré dans setup.py
-            name="camera_reader_node", # Le nom que ce noeud aura dans ros2 node list
+            package="camera_reader",
+            executable="reader_node",
+            name="camera_reader_node",
             output="screen",
-            # Vous pouvez ajouter ici des arguments (args) ou des paramètres (parameters) 
-            # si votre script reader.py en a besoin.
         ),
     ])
