fdeb73aca9287aabe0b9173696b0da7bf77392d6
jilian <jeustage@gmail.com>
2026-01-07 09:50:56 +0100

    fix: Transform in pilot

--- src/turtlebot3_pilot/turtlebot3_pilot/pilot.py
+++ src/turtlebot3_pilot/turtlebot3_pilot/pilot.py
@@ -1,17 +1,18 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
-from geometry_msgs.msg import PointStamped, PoseStamped
+from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
 from nav2_msgs.action import NavigateToPose
-
+import tf2_ros
+import math
 
 class Pilot(Node):
     def __init__(self):
         super().__init__("turtlebot3_pilot")
 
         self.goal_subscriber = self.create_subscription(
-            PointStamped, "/robot/goal_point", self.goal_callback, 10
+            PoseStamped, "/robot/goal_point", self.goal_callback, 10
         )
         self.odom_subscriber = self.create_subscription(
             Odometry, "/odom", self.odom_callback, 10
@@ -22,9 +23,15 @@ class Pilot(Node):
         self.current_pose = None
         self.goal_pose = None
 
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
+
         self.timer = self.create_timer(1, self.go_to_goal)
 
         self.get_logger().info("Pilot node started.")
+
+        self.last_sent_goal = None
+        self.distance_threshold = 0.2
 
     def odom_callback(self, msg):
         self.current_pose = msg
@@ -32,25 +39,28 @@ class Pilot(Node):
 
     def goal_callback(self, msg):
         self.goal_pose = msg
-        self.get_logger().debug(f"Received new goal point: {msg.point.x}, {msg.point.y}")
+        self.get_logger().debug(f"Received new goal point: {msg.pose.position.x}, {msg.pose.position.y}")
 
     def go_to_goal(self):
         if self.current_pose is None or self.goal_pose is None:
             self.get_logger().debug("Waiting for current pose and/or goal pose...")
             return
-        
-        self.get_logger().debug("Transforming goal point to global frame...")
 
-        pose_global = PoseStamped()
-        pose_global.header.frame_id = "map"
-        pose_global.header.stamp = self.get_clock().now().to_msg()
-        pose_global.pose.position.x = self.goal_pose.point.x + self.current_pose.pose.pose.position.x
-        pose_global.pose.position.y = self.goal_pose.point.y + self.current_pose.pose.pose.position.y
-        pose_global.pose.position.z = 0.0
-        pose_global.pose.orientation.w = 1.0
+        self.get_logger().debug("Transforming goal point to global frame...")
+        pose_global = self.tf_buffer.transform(self.goal_pose, 'map')
+
+        if self.last_sent_goal is not None:
+                dx = pose_global.pose.position.x - self.last_sent_goal.pose.position.x
+                dy = pose_global.pose.position.y - self.last_sent_goal.pose.position.y
+                dist = math.sqrt(dx**2 + dy**2)
+                
+                if dist < self.distance_threshold:
+                    self.get_logger().debug("New goal is too close to the last sent goal. Not sending a new goal.")
+                    return  
 
         self.get_logger().info(f"Sending goal ({pose_global.pose.position.x:.2f}, {pose_global.pose.position.y:.2f}) to Nav2...")
 
+        self.last_sent_goal = pose_global
         self._action_client.wait_for_server()
         goal_msg = NavigateToPose.Goal()
         goal_msg.pose = pose_global
